ea01e5381f3d96437340c6b1207b88c20023efb7
IhsanSonz <70931701+IhsanSonz@users.noreply.github.com>
2023-07-23 09:45:29 +0700

    fix: response of both success and error return (#1)

--- src/util/formatResponse.ts
+++ src/util/formatResponse.ts
@@ -1,13 +1,18 @@
 import { Response } from 'express';
-import { ResponseType } from '../types/response';
-import { ResponseTypeDefault } from '../types/response';
+import { ResponseType, ResponseTypeDefault } from '../types/response';
 
-export const formatResponse = <T>(res: Response, data?: T, message?: string, success?: boolean): void => {
+export const formatResponse = <T>(
+  res: Response,
+  data?: T,
+  message?: string,
+  success?: boolean,
+): void => {
   let response = ResponseTypeDefault as ResponseType<T>;
 
   if (data) {
     response = {
-      ...response,
+      message: message ?? response.message,
+      success: success ?? response.success,
       data,
     } as ResponseType<T>;
   }
